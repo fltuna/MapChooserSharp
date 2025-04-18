2ec9caf96b530318c642e8c0e6ebaf4bef967b92
faketuna <faketuna.st@gmail.com>
2025-04-18 15:46:02 +0900

    add: implement McsMapCycleController

---
+++ MapChooserSharp.API/Events/MapVote/McsNextMapConfirmedEvent.cs
@@ -0,0 +1,8 @@
+﻿using MapChooserSharp.API.MapConfig;
+
+namespace MapChooserSharp.API.Events.MapVote;
+
+public class McsNextMapConfirmedEvent(string modulePrefix, IMapConfig mapConfig): McsEventParam(modulePrefix), IMcsEventNoResult
+{
+    public IMapConfig MapConfig { get; } = mapConfig;
+}
---
+++ MapChooserSharp/Modules/MapCycle/McsMapCycleController.cs
@@ -0,0 +1,198 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Translations;
+using CounterStrikeSharp.API.Modules.Cvars;
+using CounterStrikeSharp.API.Modules.Cvars.Validators;
+using CounterStrikeSharp.API.Modules.Entities;
+using MapChooserSharp.API.Events.MapVote;
+using MapChooserSharp.API.MapConfig;
+using MapChooserSharp.Modules.EventManager;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TNCSSPluginFoundation.Models.Plugin;
+using TNCSSPluginFoundation.Utils.Entity;
+using TNCSSPluginFoundation.Utils.Other;
+
+namespace MapChooserSharp.Modules.MapCycle;
+
+public sealed class McsMapCycleController(IServiceProvider serviceProvider) : PluginModuleBase(serviceProvider)
+{
+    public override string PluginModuleName => "McsMapCycleController";
+    public override string ModuleChatPrefix => "McsMapCycleController";
+
+    private McsEventManager _mcsEventManager = null!;
+
+
+
+    public FakeConVar<int> NextMapTransitionDelay = new("mcs_nextmap_transition_delay", "", 10, ConVarFlags.FCVAR_NONE,
+        new RangeValidator<int>(0, 30));
+    
+    
+    
+    public int InvalidTimeLeft { get; } = -1;
+    
+    private ConVar? mp_timelimit = null;
+
+    public int TimeLeft
+    {
+        get
+        {
+            var gameRules = EntityUtil.GetGameRules();
+            if (gameRules == null)
+            {
+                Logger.LogError("Failed to find the Game Rules entity!");
+                return InvalidTimeLeft;
+            }
+
+            if (mp_timelimit == null)
+            {
+                mp_timelimit = ConVar.Find("mp_timelimit");
+                if (mp_timelimit == null)
+                {
+                    Logger.LogWarning("Failed to find the mp_timelimit ConVar and try to find again.");
+                    return InvalidTimeLeft;
+                }
+            }
+
+            var timeLimit = mp_timelimit.GetPrimitiveValue<float>();
+            if (timeLimit < 0.001f)
+            {
+                return 0;
+            }
+
+            return (int)((gameRules.GameStartTime + timeLimit * 60.0f) - Server.CurrentTime);
+        }
+    }
+
+    
+    
+    public override void RegisterServices(IServiceCollection services)
+    {
+        services.AddSingleton(this);
+    }
+
+    protected override void OnInitialize()
+    {
+
+    }
+
+    protected override void OnAllPluginsLoaded()
+    {
+        _mcsEventManager = ServiceProvider.GetRequiredService<McsEventManager>();
+        
+        _mcsEventManager.RegisterEventHandler<McsNextMapConfirmedEvent>(OnNextMapConfirmed);
+        
+        Plugin.RegisterListener<Listeners.OnMapStart>(OnMapStart);
+        Plugin.RegisterEventHandler<EventRoundEnd>(OnRoundEnd);
+    }
+
+    protected override void OnUnloadModule()
+    {
+        _mcsEventManager.UnregisterEventHandler<McsNextMapConfirmedEvent>(OnNextMapConfirmed);
+        
+        Plugin.RemoveListener<Listeners.OnMapStart>(OnMapStart);
+        Plugin.DeregisterEventHandler<EventRoundEnd>(OnRoundEnd);
+    }
+
+    private IMapConfig? NextMap { get; set; }
+
+    public void SetNextMap(IMapConfig nextMap)
+    {
+        NextMap = nextMap;
+    }
+
+    public IMapConfig? GetNextMap()
+    {
+        return NextMap;
+    }
+
+    internal void ChangeToNextMap()
+    {
+        if (NextMap == null)
+        {
+            Logger.LogError("Failed to change map: next map is null");
+            return;
+        }
+
+        long workshopId = NextMap.WorkshopId;
+
+        if (workshopId == 0)
+        {
+            DebugLogger.LogInformation(
+                $"No workshop ID defined! We will try -> ds_workshop_changelevel {NextMap.MapName}");
+            MapUtil.ChangeMap(NextMap.MapName);
+            return;
+        }
+
+        DebugLogger.LogInformation($"We will try to change map to {NextMap.MapName} with workshop ID: {workshopId}");
+        MapUtil.ChangeToWorkshopMap(workshopId);
+    }
+
+
+    private void OnMapStart(string mapName)
+    {
+        NextMap = null;
+    }
+
+    private HookResult OnRoundEnd(EventRoundEnd @event, GameEventInfo info)
+    {
+
+        return HookResult.Continue;
+    }
+
+
+    private void OnNextMapConfirmed(McsNextMapConfirmedEvent @event)
+    {
+        NextMap = @event.MapConfig;
+    }
+    
+
+    public string GetFormattedTimeLeft(int timeLeft)
+    {
+        int hours = timeLeft / 3600;
+        int minutes = (timeLeft % 3600) / 60;
+        int seconds = timeLeft % 60;
+
+        if (hours > 0)
+        {
+            return $"{hours} {(hours == 1 ? "hour" : "hours")} " +
+                   $"{minutes} {(minutes == 1 ? "minute" : "minutes")} " +
+                   $"{seconds} {(seconds == 1 ? "second" : "seconds")}";
+        }
+
+        if (minutes > 0)
+        {
+            return $"{minutes} {(minutes == 1 ? "minute" : "minutes")} " +
+                   $"{seconds} {(seconds == 1 ? "second" : "seconds")}";
+        }
+
+        return $"{seconds} {(seconds == 1 ? "second" : "seconds")}";
+    }
+
+
+    public string GetFormattedTimeLeft(int timeLeft, CCSPlayerController player)
+    {
+        SteamID? steamId = player.AuthorizedSteamID;
+        if (steamId == null)
+            return GetFormattedTimeLeft(timeLeft);
+        
+        int hours = timeLeft / 3600;
+        int minutes = (timeLeft % 3600) / 60;
+        int seconds = timeLeft % 60;
+
+        var playerCulture = PlayerLanguageManager.Instance.GetLanguage(steamId);
+        using var tempCulture = new WithTemporaryCulture(playerCulture);
+
+        if (hours > 0)
+        {
+            return "TODO_TRANSLATE| HOURS";
+        }
+
+        if (minutes > 0)
+        {
+            return "TODO_TRANSLATE| MINUETS";
+        }
+
+        return "TODO_TRANSLATE| SECONDS";
+    }
+}
