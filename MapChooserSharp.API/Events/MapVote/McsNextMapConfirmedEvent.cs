8e4d1043833466b309effa61ed11c6d22e2f92d5
faketuna <faketuna.st@gmail.com>
2025-04-26 17:57:01 +0900

    add: add missing code documentation

--- MapChooserSharp.API/Events/MapVote/McsNextMapConfirmedEvent.cs
+++ MapChooserSharp.API/Events/MapVote/McsNextMapConfirmedEvent.cs
@@ -2,7 +2,13 @@
 
 namespace MapChooserSharp.API.Events.MapVote;
 
+/// <summary>
+/// This event will be called when after RTV vote with "don't change map"
+/// </summary>
 public class McsNextMapConfirmedEvent(string modulePrefix, IMapConfig mapConfig): McsEventParam(modulePrefix), IMcsEventNoResult
 {
+    /// <summary>
+    /// Map config data
+    /// </summary>
     public IMapConfig MapConfig { get; } = mapConfig;
 }
