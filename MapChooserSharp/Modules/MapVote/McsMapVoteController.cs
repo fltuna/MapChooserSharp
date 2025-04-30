0274c2fa0bf349b29de843809f64276e61a1f46b
faketuna <faketuna.st@gmail.com>
2025-04-30 20:55:10 +0900

    modify: fix random map pickup conditions

--- MapChooserSharp/Modules/MapVote/McsMapVoteController.cs
+++ MapChooserSharp/Modules/MapVote/McsMapVoteController.cs
@@ -265,11 +265,11 @@ internal sealed class McsMapVoteController(IServiceProvider serviceProvider) : P
                 .Where(map => map.Value.MapCooldown.CurrentCooldown <= 0)
                 .Where(map => !map.Value.OnlyNomination)
                 .Where(map => !map.Value.NominationConfig.RestrictToAllowedUsersOnly)
-                .Where(map => map.Value.NominationConfig.MinPlayers <= Utilities.GetPlayers().Count(p => p is { IsBot: false, IsHLTV: false }))
-                .Where(map => map.Value.NominationConfig.MaxPlayers > Utilities.GetPlayers().Count(p => p is { IsBot: false, IsHLTV: false }))
+                .Where(map => map.Value.NominationConfig.MinPlayers == 0 || map.Value.NominationConfig.MinPlayers <= Utilities.GetPlayers().Count(p => p is { IsBot: false, IsHLTV: false }))
+                .Where(map => map.Value.NominationConfig.MaxPlayers == 0 || map.Value.NominationConfig.MaxPlayers > Utilities.GetPlayers().Count(p => p is { IsBot: false, IsHLTV: false }))
                 .Where(map => !map.Value.NominationConfig.RequiredPermissions.Any())
-                .Where(map => map.Value.NominationConfig.DaysAllowed.Any() && map.Value.NominationConfig.DaysAllowed.Contains(DateTime.Today.DayOfWeek) )
-                .Where(map => map.Value.NominationConfig.AllowedTimeRanges.Any() && map.Value.NominationConfig.AllowedTimeRanges.Count(range => range.IsInRange(TimeOnly.FromDateTime(DateTime.Now))) < 1 )
+                .Where(map => !map.Value.NominationConfig.DaysAllowed.Any() || map.Value.NominationConfig.DaysAllowed.Contains(DateTime.Today.DayOfWeek) )
+                .Where(map => !map.Value.NominationConfig.AllowedTimeRanges.Any() || map.Value.NominationConfig.AllowedTimeRanges.Count(range => range.IsInRange(TimeOnly.FromDateTime(DateTime.Now))) < 1 )
                 .Take(numToPick);
 
             foreach (var (key, value) in pickedMaps)
