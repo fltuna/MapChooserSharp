a4bb9133a98dd9bda2457ce1bf9076e1977ac30e
faketuna <faketuna.st@gmail.com>
2025-05-02 22:50:35 +0900

    add: add code doc

--- MapChooserSharp.API/Events/MapCycle/McsNextMapRemovedEvent.cs
+++ MapChooserSharp.API/Events/MapCycle/McsNextMapRemovedEvent.cs
@@ -2,9 +2,16 @@
 
 namespace MapChooserSharp.API.Events.MapCycle;
 
-
+/// <summary>
+/// This event will be called when next map is removed
+/// </summary>
+/// <param name="modulePrefix">Module Prefix</param>
+/// <param name="previousNextMap">Previous map that was set</param>
 public class McsNextMapRemovedEvent(string modulePrefix, IMapConfig previousNextMap): McsEventParam(modulePrefix), IMcsEventNoResult
 {
     
+    /// <summary>
+    /// The previous map that was set.
+    /// </summary>
     public IMapConfig PreviousNextMap { get; } = previousNextMap;
 }
