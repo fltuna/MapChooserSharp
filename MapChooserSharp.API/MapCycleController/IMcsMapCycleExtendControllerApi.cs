e765324d28a56d974ece6a775fdaaf701aa19e1e
faketuna <faketuna.st@gmail.com>
2025-05-13 14:24:47 +0900

    modify: a little documentation fix

--- MapChooserSharp.API/MapCycleController/IMcsMapCycleExtendControllerApi.cs
+++ MapChooserSharp.API/MapCycleController/IMcsMapCycleExtendControllerApi.cs
@@ -6,9 +6,9 @@
 public interface IMcsMapCycleExtendControllerApi
 {
     /// <summary>
-    /// Extends a current map.
+    /// Extends a current map by specified time.
     /// </summary>
-    /// <param name="extendTime">extends a round, timelimit and roundtime. It depends on a map type</param>
+    /// <param name="extendTime">extends a round, timelimit or roundtime. It depends on a map type</param>
     /// <returns>McsMapCycleExtendResult.Extended if map is extended successfully</returns>
     public McsMapCycleExtendResult ExtendCurrentMap(int extendTime);
 }
